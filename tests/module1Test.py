d5ee83d4cb47d478c1c9ef8ef0bb0e05da2d9fc2
david.fitzgerald <david.fitzgerald@live.co.uk>
2025-09-08 12:16:12 +0100

    refactor test file

--- tests/module1Test.py
+++ tests/module1Test.py
@@ -4,16 +4,15 @@ from someModule import module1
 class TestModule1(unittest.TestCase):
 	def setUp(self):
 		print("doing setup")
+		uut = module1.module1()
 
 	def tearDown(self):
 		print("doing teardown")
 
 	def testTruePass(self):
-		uut = module1.module1()
 		self.assertEqual(uut.returnsTrue(), True)
 
 	def testFalsePass(self):
-		uut = module1.module1()
 		self.assertEqual(uut.returnsFalse(), False)
 
 if __name__ == '__main__':
